0676476019793ade24ac9e25da497eadee4ac6a2
Marco Aurélio <thecoreh@gmail.com>
2012-02-12 17:12:11 -0200

    Update files to reflect new project name.

--- test/unit.js
+++ test/unit.js
@@ -1,53 +1,53 @@
-var oa = require('../lib/oa')
+var webd = require('../lib/webd')
 var expect = require('expect.js')
 
-describe('oa.Unit', function() {
+describe('webd.Unit', function() {
   it('should be a constructor', function() {
-    expect(oa.Unit).to.be.a(Function)
+    expect(webd.Unit).to.be.a(Function)
   })
   it('should create correct Units when given numerators and denominators', function() {
-    var result = new oa.Unit(['m'],['s'])
-    expect(result).to.be.a(oa.Unit)
+    var result = new webd.Unit(['m'],['s'])
+    expect(result).to.be.a(webd.Unit)
     expect(result.numerator).to.be.eql(['m'])
     expect(result.denominator).to.be.eql(['s'])
   })
   it('should accept numerators and denominators outside of arrays', function() {
-    var result = new oa.Unit('m', 's')
-    expect(result).to.be.a(oa.Unit)
+    var result = new webd.Unit('m', 's')
+    expect(result).to.be.a(webd.Unit)
     expect(result.numerator).to.be.eql(['m'])
     expect(result.denominator).to.be.eql(['s'])
   })
   it('should accept empty numerators', function() {
-    var result = new oa.Unit([], ['s']) // Hertz
-    expect(result).to.be.a(oa.Unit)
+    var result = new webd.Unit([], ['s']) // Hertz
+    expect(result).to.be.a(webd.Unit)
     expect(result.numerator).to.be.eql([])
     expect(result.denominator).to.be.eql(['s'])
   })
   it('should accept empty denominators', function() {
-    var result = new oa.Unit(['s'], []) // Seconds
-    expect(result).to.be.a(oa.Unit)
+    var result = new webd.Unit(['s'], []) // Seconds
+    expect(result).to.be.a(webd.Unit)
     expect(result.numerator).to.be.eql(['s'])
     expect(result.denominator).to.be.eql([])
   })
   it('should convert properly to a string', function() {
-    expect(new oa.Unit(['m'], ['s']).toString()).to.be('m/s')
-    expect(new oa.Unit([], ['s']).toString()).to.be('1/s')
-    expect(new oa.Unit(['s'], []).toString()).to.be('s')
-    expect(new oa.Unit([], []).toString()).to.be('1')
+    expect(new webd.Unit(['m'], ['s']).toString()).to.be('m/s')
+    expect(new webd.Unit([], ['s']).toString()).to.be('1/s')
+    expect(new webd.Unit(['s'], []).toString()).to.be('s')
+    expect(new webd.Unit([], []).toString()).to.be('1')
   })
   it('should multiply correctly', function() {
-    var u1 = new oa.Unit('m', 's')
-    var u2 = new oa.Unit('atoms', 'kg')
+    var u1 = new webd.Unit('m', 's')
+    var u2 = new webd.Unit('atoms', 'kg')
     var u3 = u1.multiply(u2)
-    expect(u3).to.be.a(oa.Unit)
+    expect(u3).to.be.a(webd.Unit)
     expect(u3.numerator).to.be.eql(['atoms', 'm'])
     expect(u3.denominator).to.be.eql(['kg', 's'])
   })
   it('should simplify correctly', function() {
-    var u1 = new oa.Unit('m', ['s', 's'])
-    var u2 = new oa.Unit('s')
+    var u1 = new webd.Unit('m', ['s', 's'])
+    var u2 = new webd.Unit('s')
     var u3 = u1.multiply(u2)
-    expect(u3).to.be.a(oa.Unit)
+    expect(u3).to.be.a(webd.Unit)
     expect(u3.numerator).to.be.eql(['m'])
     expect(u3.denominator).to.be.eql(['s'])
   })
--- test/webd.js
+++ test/webd.js
@@ -1,55 +1,55 @@
-var oa = require('../lib/oa')
+var webd = require('../lib/webd')
 var expect = require('expect.js')
 
-describe('OpenAnything', function() {
+describe('WebD', function() {
   it('should export a function named parse', function() {
-    expect(oa.parse).to.be.a(Function)
+    expect(webd.parse).to.be.a(Function)
   })
   it('should parse dates', function() {
-    var result = oa.parse(
-      { _: 'oa:date'
+    var result = webd.parse(
+      { _: 'webd:date'
       , value: '2012-02-05T21:43:51.173Z'
       }
     )
     expect(result).to.be.a(Date)
   })
   it('should parse units', function() {
-    var result = oa.parse(
-      { _: 'oa:unit'
+    var result = webd.parse(
+      { _: 'webd:unit'
       , numerator: ['m']
       , denominator: ['s']
       }
     )
-    expect(result).to.be.a(oa.Unit)
+    expect(result).to.be.a(webd.Unit)
     expect(result.numerator).to.be.eql(['m'])
     expect(result.denominator).to.be.eql(['s'])
   })
   it('should parse measures', function() {
-    var result = oa.parse(
-      { _: 'oa:measure'
+    var result = webd.parse(
+      { _: 'webd:measure'
       , unit:
-        { _: 'oa:unit'
+        { _: 'webd:unit'
         , numerator: ['m']
         , denominator: ['s']
         }
       , value: 0.5
       }
     )
-    expect(result).to.be.a(oa.Measure)
+    expect(result).to.be.a(webd.Measure)
     expect(result.value).to.be.a('number')
-    expect(result.unit).to.be.a(oa.Unit)
+    expect(result.unit).to.be.a(webd.Unit)
   })
   it('should export a function named stringify', function() {
-    expect(oa.stringify).to.be.a(Function);
+    expect(webd.stringify).to.be.a(Function);
   })
   it('should stringify dates', function() {
     var now = new Date()
-    expect(oa.stringify(now)).to.be('{"_":"oa:date","value":"' + now.toJSON() + '"}')
+    expect(webd.stringify(now)).to.be('{"_":"webd:date","value":"' + now.toJSON() + '"}')
   })
   it('should stringify units', function() {
-    expect(oa.stringify(new oa.Unit('m', 's'))).to.be('{"_":"oa:unit","numerator":["m"],"denominator":["s"]}')
+    expect(webd.stringify(new webd.Unit('m', 's'))).to.be('{"_":"webd:unit","numerator":["m"],"denominator":["s"]}')
   })
   it('should stringify measures', function() {
-    expect(oa.stringify(new oa.Measure(5, new oa.Unit('m', 's')))).to.be('{"_":"oa:measure","value":5,"unit":{"_":"oa:unit","numerator":["m"],"denominator":["s"]}}')
+    expect(webd.stringify(new webd.Measure(5, new webd.Unit('m', 's')))).to.be('{"_":"webd:measure","value":5,"unit":{"_":"webd:unit","numerator":["m"],"denominator":["s"]}}')
   })
 })
